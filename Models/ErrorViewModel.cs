18fc93538956ab77b89c143ccbf78509a6b72f9d
AmineC95 <amine.chioukh95@gmail.com>
2022-07-20 19:38:52 +0200

    v1.0

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Rainy_Play.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
